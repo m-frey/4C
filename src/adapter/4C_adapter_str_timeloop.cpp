daa5338bff721e2f331393aaadc981bef08cedee
Moritz Frey <moritz.frey@unibw.de>
2026-02-11 16:34:16 +0100

    Add F_MACRO input

--- src/adapter/4C_adapter_str_timeloop.cpp
+++ src/adapter/4C_adapter_str_timeloop.cpp
@@ -9,6 +9,8 @@
 
 #include "4C_global_data.hpp"
 #include "4C_inpar_structure.hpp"
+#include "4C_io_input_field.hpp"
+#include "4C_linalg_tensor.hpp"
 
 #include <Teuchos_StandardParameterEntryValidators.hpp>
 
@@ -19,6 +21,34 @@ FOUR_C_NAMESPACE_OPEN
 /*----------------------------------------------------------------------*/
 int Adapter::StructureTimeLoop::integrate()
 {
+  // Print optional user-provided deformation gradient once, if present
+  {
+    static bool defgrad_printed = false;
+    if (!defgrad_printed)
+    {
+      const auto& sdyn = Global::Problem::instance()->structural_dynamic_params();
+      if (sdyn.isParameter("DEFGRAD"))
+      {
+        const auto defgrad_field =
+            sdyn.get<Core::IO::InterpolatedInputField<Core::LinAlg::Tensor<double, 3, 3>>>(
+                "DEFGRAD");
+        const std::array<double, 3> xi{0.0, 0.0, 0.0};
+        const auto defgrad = defgrad_field.interpolate(0, xi, "DEFGRAD");
+        std::cout << "STRUCTURAL DYNAMIC/DEFGRAD:" << std::endl;
+        for (int i = 0; i < 3; ++i)
+        {
+          std::cout << "  ";
+          for (int j = 0; j < 3; ++j)
+          {
+            std::cout << defgrad(i, j) << (j < 2 ? " " : "");
+          }
+          std::cout << std::endl;
+        }
+      }
+      defgrad_printed = true;
+    }
+  }
+
   // error checking variables
   Inpar::Solid::ConvergenceStatus convergencestatus = Inpar::Solid::conv_success;
 
